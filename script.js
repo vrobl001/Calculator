82db50010e7a3ce4409326c700d1cf5a320c8677
Vincent Robles <sylkisgsx@gmail.com>
2020-02-23 01:23:51 -0500

    created handle function and negative numbers

--- script.js
+++ script.js
@@ -5,83 +5,113 @@ const decimalButton = document.querySelector('[data-decimal]');
 const clearAllButton = document.querySelector('[data-clear-all]');
 const previousOperand = document.querySelector('#previous-operand');
 const currentOperand = document.querySelector('#current-operand');
-let storePreviousOperand;
-let needsReset;
-let operation = "";
+let needsReset = '';
+let operation = '';
+
+function clearAll() {
+    previousOperand.innerHTML = '';
+    currentOperand.innerHTML = '';
+    operation = '';
+}
 
 function handleDisplay() {
-    if(currentOperand.innerHTML) {
-        currentOperand.innerHTML = parseFloat(currentOperand.innerHTML.replace(/[^0-9]/g, ""));
-        currentOperand.innerHTML = Number(currentOperand.innerHTML).toLocaleString('en');
+    if (currentOperand.innerHTML) {
+        currentOperand.innerHTML = parseFloat(
+            currentOperand.innerHTML.replace(/,/g, '')
+        ).toLocaleString('en');
     }
+}
+
+function handleNeedsReset() {
+    currentOperand.innerHTML = '';
+    operation = '';
+    needsReset = 'false';
+}
+
+function handleNegNum() {
+    currentOperand.innerHTML = '-';
 }
 
 numberButtons.forEach(button => {
     button.addEventListener('click', () => {
-        if(currentOperand.innerHTML.length > 13) return;
+        if (needsReset == 'true') handleNeedsReset();
+        if (currentOperand.innerHTML.length > 13) return;
         currentOperand.innerHTML = currentOperand.innerHTML + button.innerHTML;
         handleDisplay();
     });
-})
+});
 
 operationButtons.forEach(button => {
     button.addEventListener('click', () => {
-        operation = button.innerHTML
-        previousOperand.innerHTML = currentOperand.innerHTML;
-        currentOperand.innerHTML = "";
+        // Allows user to enter a negative number after a computation
+        if (needsReset === 'true') handleNeedsReset();
+        // Allows user to enter a negative number
+        if (button.innerHTML === '-') {
+            if (currentOperand.innerHTML === '-') return;
+            if (!currentOperand.innerHTML) {
+                handleNegNum();
+                return;
+            }
+        }
+        // Allows a user to create a previous operand only if the current operand exists and is a number
+        if (
+            !isNaN(currentOperand.innerHTML) &&
+            currentOperand.innerHTML &&
+            !previousOperand.innerHTML
+        ) {
+            operation = button.innerHTML;
+            previousOperand.innerHTML = `${currentOperand.innerHTML} ${operation}`;
+            currentOperand.innerHTML = '';
+        }
     });
-})
+});
 
 clearAllButton.addEventListener('click', button => {
-    clearAll()
-})
+    clearAll();
+});
 
 equalsButton.addEventListener('click', button => {
-    compute()
-})
+    needsReset = 'true';
+    compute();
+});
 
 decimalButton.addEventListener('click', button => {
-    if(!currentOperand.innerHTML.includes(".")) {
-        currentOperand.innerHTML = currentOperand.innerHTML + "."
+    // Prevents user from entering more than one decimal
+    if (!currentOperand.innerHTML.includes('.')) {
+        currentOperand.innerHTML = currentOperand.innerHTML + '.';
     }
-})
-
-function clearAll() {
-    previousOperand.innerHTML = "";
-    currentOperand.innerHTML = "";
-    operation = "";
-}
+});
 
 function compute() {
-    const firstNum = parseFloat(previousOperand.innerHTML.replace(/[^0-9]/g, ""));
-    const secondNum = parseFloat(currentOperand.innerHTML.replace(/[^0-9]/g, ""));
+    const firstNum = parseFloat(previousOperand.innerHTML.replace(/,/g, ''));
+    const secondNum = parseFloat(currentOperand.innerHTML.replace(/,/g, ''));
     switch (operation) {
         case '+':
             total = firstNum + secondNum;
             currentOperand.innerHTML = total.toLocaleString('en');
-            previousOperand.innerHTML = "";   
-        break;
-        
+            previousOperand.innerHTML = '';
+            break;
+
         case '-':
             total = firstNum - secondNum;
             currentOperand.innerHTML = total.toLocaleString('en');
-            previousOperand.innerHTML = ""; 
-        break;
+            previousOperand.innerHTML = '';
+            break;
 
         case 'x':
             total = firstNum * secondNum;
             currentOperand.innerHTML = total.toLocaleString('en');
-            previousOperand.innerHTML = ""; 
-        break;
-        
+            previousOperand.innerHTML = '';
+            break;
+
         case '÷':
             total = firstNum / secondNum;
             currentOperand.innerHTML = total.toLocaleString('en');
-            previousOperand.innerHTML = ""; 
-        break;
-    
-        default:""
+            previousOperand.innerHTML = '';
+            break;
+
+        default:
+            '';
             break;
     }
-
 }
